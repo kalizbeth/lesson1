f8917ec01ef50bc5e71dc810d69b9b62cdaed77c
unknown <kalizbeth.akinyi@gmail.com>
2024-11-07 12:07:31 +0300

    first commit

---
+++ auths/te.py
@@ -0,0 +1,16 @@
+# aplaction => criteria 
+
+# creteria\;
+#   apis,
+#   ui/frented
+#   database
+  
+  
+#   api=>
+#   methods request =data from the db;
+  
+#   methods:
+#       POST: save 
+#       |GET: getting posted data from the db
+#       Delete|: delete
+#       PATCH/PUT : Updating;
